b8c854b2a7b7252bad46c84e5d1c0b1d2da2f136
Md Aljubayer <tanzimnahid6@gmail.com>
2024-02-27 21:42:40 +0600

    card add

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,10 +3,10 @@ import animation from "../assets/animation.json";
 const Banner = () => {
   return (
     <div className="md:flex justify-around gap-2 p-4 h-[450px] mt-16">
-      <div className="md:w-1/2 md:order-2  border border-red-100">
+      <div className="md:w-1/2 md:order-2  ">
         <Lottie animationData={animation} className="h-96"></Lottie>
       </div>
-      <div className="md:w-1/2 md:order-1  border border-red-100">
+      <div className="md:w-1/2 md:order-1  ">
         <p className="text-4xl md:text-7xl font-bold  bg-gradient-to-r from-[#1e6162] to-[#5cc4a8] bg-clip-text text-transparent leading-1">
           Elevate Your Experience: AI Magic in Every Click
         </p>
