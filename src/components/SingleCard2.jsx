7382d8e6bd439ae6c0921ce12120713f7d2903e3
Md Aljubayer <tanzimnahid6@gmail.com>
2024-02-29 23:04:36 +0600

    button add

--- src/components/SingleCard2.jsx
+++ src/components/SingleCard2.jsx
@@ -44,7 +44,7 @@ const SingleCard2 = ({ project }) => {
               type="button"
               onClick={toggleShowMore}
             >
-              Learn More
+              Model
             </button>
           </Link>
 
@@ -53,7 +53,7 @@ const SingleCard2 = ({ project }) => {
               className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-green-600 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
               type="button"
             >
-              Github
+              Output
             </button>
           </a>
         </div>
