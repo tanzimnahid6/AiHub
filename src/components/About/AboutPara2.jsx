7d4dc9854c1e11aba1be98fdc0855657b255d4da
Md Aljubayer <tanzimnahid6@gmail.com>
2024-03-06 13:04:47 +0600

    heading and font chnage

--- src/components/About/AboutPara2.jsx
+++ src/components/About/AboutPara2.jsx
@@ -2,39 +2,46 @@
 
 
 const AboutUSPara2 = () => {
-    return (
+  return (
+    <div>
+      <div className="flex flex-col sm:flex-row md:flex-row-reverse  md:my-28 lg:gap-20 md:gap-10">
+        <img
+          src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+          alt=""
+          className="w-[410px] md:w-[300px] rounded-md object-cover border-t-8 border-l-8 border-[#236A68] transition delay-150 duration-300 ease-in-out hover:scale-90 hidden md:block "
+        />
         <div>
-          <div className="flex flex-col sm:flex-row md:flex-row-reverse  md:my-28 lg:gap-20 md:gap-10">
-      <img
-        src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
-        alt=""
-        className="w-[410px] md:w-[300px] rounded-md object-cover border-t-8 border-l-8 border-[#236A68] transition delay-150 duration-300 ease-in-out hover:scale-90 hidden md:block "
-      />
-      <div>
-        <h2 className="text-xl md:text-2xl lg:text-4xl font-bold  text-[#236A68] hidden md:block">
-        Partnering for AI Excellence
-        </h2>
+          <h2 className="text-xl md:text-2xl lg:text-4xl font-bold  text-[#236A68] hidden md:block">
+            Partnering for AI Excellence
+          </h2>
 
-        <ul className="list-image text-sm">
-          <li className="flex gap-4 py-3">
+          <ul className="list-image ">
             <li className="flex gap-4 py-3">
-            From strategic consulting to hands-on implementation and ongoing support, we guide businesses through every stage of their AI journey. Whether you're looking to optimize processes, enhance customer experiences, or gain actionable insights from data, we have the tools, expertise, and dedication to help you succeed.
+              From strategic consulting to hands-on implementation and ongoing
+              support, we guide businesses through every stage of their AI
+              journey. Whether you're looking to optimize processes, enhance
+              customer experiences, or gain actionable insights from data, we
+              have the tools, expertise, and dedication to help you succeed.
             </li>
-          </li>
 
-          <li className="flex gap-4">
-          At BizNest AI, we're not just a service provider – we're your trusted partner committed to your success. We work closely with our clients to understand their goals, challenges, and opportunities, and we tailor our solutions to drive meaningful and sustainable results.
-          </li>
-          <li className="flex gap-4 py-3">
-          Join us on this exciting journey as we unlock the full potential of AI for businesses worldwide. Explore our website to learn more about our services, expertise, and success stories, and discover how BizNest AI can help your business thrive in the age of AI.
-          </li>
-
-        
-        </ul>
-      </div>
-    </div>  
+            <li className="flex gap-4">
+              At BizNest AI, we're not just a service provider – we're your
+              trusted partner committed to your success. We work closely with
+              our clients to understand their goals, challenges, and
+              opportunities, and we tailor our solutions to drive meaningful and
+              sustainable results.
+            </li>
+            <li className="flex gap-4 py-3">
+              Join us on this exciting journey as we unlock the full potential
+              of AI for businesses worldwide. Explore our website to learn more
+              about our services, expertise, and success stories, and discover
+              how BizNest AI can help your business thrive in the age of AI.
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AboutUSPara2;
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,53 +1,117 @@
-
-
 const Accordion = () => {
   return (
     <div className="my-12">
-        <h1 className="text-center font-bold text-3xl md:text-5xl my-4">FAQs</h1>
+      <h1 className="text-center font-bold text-3xl md:text-5xl my-4">FAQs</h1>
       <div className="join join-vertical w-full">
         <div className="collapse collapse-arrow join-item border border-base-300">
           <input type="radio" name="my-accordion-4" defaultChecked />
           <div className="collapse-title text-xl font-medium">
-         1.What is BizNest AI?
+            1.What is BizNest AI?
           </div>
           <div className="collapse-content">
-            <p>BizNest AI is a leading provider of Artificial Intelligence solutions and services aimed at helping businesses worldwide leverage AI technologies to drive growth, innovation, and success.</p>
+            <p>
+              BizNest AI is a leading provider of Artificial Intelligence
+              solutions and services aimed at helping businesses worldwide
+              leverage AI technologies to drive growth, innovation, and success.
+            </p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border border-base-300">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-           2. Who can benefit from BizNest AI services?
+            2. Who can benefit from BizNest AI services?
           </div>
           <div className="collapse-content">
-            <p>Our services are designed to cater to both new and existing businesses seeking to integrate AI into their operations, as well as individuals interested in learning how to apply AI in a business context.</p>
+            <p>
+              Our services are designed to cater to both new and existing
+              businesses seeking to integrate AI into their operations, as well
+              as individuals interested in learning how to apply AI in a
+              business context.
+            </p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border border-base-300">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-           3. What types of AI solutions does BizNest AI offer?
+            3. What types of AI solutions does BizNest AI offer?
           </div>
           <div className="collapse-content">
-            <p>We offer a wide range of AI solutions, including consulting services, implementation and integration, training and education, solution development, and ongoing support and maintenance.</p>
+            <p>
+              We offer a wide range of AI solutions, including consulting
+              services, implementation and integration, training and education,
+              solution development, and ongoing support and maintenance.
+            </p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border border-base-300">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-           4. How can I get started with BizNest AI?
+            4. How can I get started with BizNest AI?
           </div>
           <div className="collapse-content">
-            <p>To get started, simply visit our website and explore our services. You can contact us directly to discuss your specific needs and objectives, and our team will work with you to develop a tailored solution.</p>
+            <p>
+              To get started, simply visit our website and explore our services.
+              You can contact us directly to discuss your specific needs and
+              objectives, and our team will work with you to develop a tailored
+              solution.
+            </p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border border-base-300">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-           5. Is my data safe with BizNest AI?
+            5. Is my data safe with BizNest AI?
           </div>
           <div className="collapse-content">
-            <p>Yes, we are committed to safeguarding the privacy and security of your personal information. We implement appropriate measures to protect data against unauthorized access, alteration, disclosure, or destruction.</p>
+            <p>
+              Absolutely! We offer demos and consultations to interested parties
+              to demonstrate our capabilities and discuss how AI can benefit
+              their business. Contact us to schedule a demo or consultation
+              today.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            6. Do you provide training for individuals interested in learning AI
+            with business?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Yes, we offer training programs designed to empower individuals
+              with the knowledge and skills to leverage AI for business success.
+              Our training sessions cover various AI concepts and their
+              applications in real-world business scenarios.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            7. Is my data safe with BizNest AI?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Yes, we are committed to safeguarding the privacy and security of
+              your personal information. We implement appropriate measures to
+              protect data against unauthorized access, alteration, disclosure,
+              or destruction.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            8. How often is your Privacy Policy updated?
+          </div>
+          <div className="collapse-content">
+            <p>
+            Our Privacy Policy may be updated periodically to reflect changes in our practices or regulatory requirements. Any updates will be posted on our website, and users are encouraged to review the policy regularly.
+            </p>
           </div>
         </div>
       </div>
