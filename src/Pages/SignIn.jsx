4855450ecd62260dea36b628832c00b2eb7a67aa
tanzim-nahid <tanzimnahid6@gmail.com>
2024-03-12 22:44:26 +0600

    added the sign in page

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -7,10 +7,10 @@ const SignIn = () => {
      }, []);
     return (
       <div className="h-4/6 my-10 bg-gray-100 text-gray-900 flex justify-center">
-        <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1 items-center gap-4">
+        <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex  flex-row-reverse justify-center flex-1 items-center gap-4">
           <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12 ">
             <div className="mt-12 flex flex-col items-center">
-              <h1 className="text-2xl xl:text-3xl font-extrabold">Log In</h1>
+              <h1 className="text-2xl xl:text-3xl font-extrabold">Sign In</h1>
               <div className="w-full flex-1 mt-8">
                 <div className="flex flex-col items-center">
                   <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
@@ -34,17 +34,23 @@ const SignIn = () => {
                         />
                       </svg>
                     </div>
-                    <span className="ml-4">Log In with Google</span>
+                    <span className="ml-4">Sign In with Google</span>
                   </button>
                 </div>
                 <div className="my-12 border-b text-center">
                   <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                    Or Login with E-mail
+                    Or Sign in with E-mail
                   </div>
                 </div>
                 <div className="mx-auto max-w-xs">
                   <input
                     className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="text"
+                    ref={emailInputRef}
+                    placeholder="Name"
+                  />
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                     type="email"
                     ref={emailInputRef}
                     placeholder="Email"
@@ -53,6 +59,11 @@ const SignIn = () => {
                     className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                     type="password"
                     placeholder="Password"
+                  />
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                    type="password"
+                    placeholder="Retype Password"
                   />
                   <button className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                     <svg
@@ -67,7 +78,7 @@ const SignIn = () => {
                       <circle cx="8.5" cy={7} r={4} />
                       <path d="M20 8v6M23 11h-6" />
                     </svg>
-                    <span className="ml-3">Log In </span>
+                    <span className="ml-3">Sign In </span>
                   </button>
                   <p className="mt-6 text-xs text-gray-600 text-center">
                     I agree to abide by BizNest's
